8f35134e6450cb35675ed46f8e996c2d990db1af
Phuong To <tophu95@gmail.com>
2019-07-08 16:17:49 +0100

    created movie model

---
+++ db/migrate/20190708151439_create_movies.rb
@@ -0,0 +1,14 @@
+class CreateMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :description
+      t.integer :year
+      t.integer :review
+      t.integer :price
+      t.string :genre
+
+      t.timestamps
+    end
+  end
+end
