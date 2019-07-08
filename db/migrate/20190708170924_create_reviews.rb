4a9a705dd5fc3e36c20a86a521607f625ae94b98
Phuong To <tophu95@gmail.com>
2019-07-08 22:09:56 +0100

    testing database

--- db/migrate/20190708170924_create_reviews.rb
+++ db/migrate/20190708170924_create_reviews.rb
@@ -3,8 +3,8 @@ class CreateReviews < ActiveRecord::Migration[5.2]
     create_table :reviews do |t|
       t.integer :rating
       t.references :movie_id, foreign_key: true
-      t.references :game_id, foreign_key: true
-      t.references :music_id, foreign_key: true
+      # t.references :game_id, foreign_key: true
+      # t.references :music_id, foreign_key: true
 
       t.timestamps
     end
