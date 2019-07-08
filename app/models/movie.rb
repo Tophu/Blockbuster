783a83266ec832f1c76315b1800e9c19b28db504
Phuong To <tophu95@gmail.com>
2019-07-08 16:29:33 +0100

    validations for model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ApplicationRecord
+  belongs_to :user, class_name: :Order, foreign_key: 'movie_id'
+  monetize :price_cents
+
+include PgSearch
+ pg_search_scope :search_by_product_name_and_year,
+         against: [:title, :year],
+         using: {
+          tsearch: { prefix: true }
+         }
 end
