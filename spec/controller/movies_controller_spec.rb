cd676356a1d684a3f81075d138edf5cbde1df403
Phuong To <tophu95@gmail.com>
2019-07-09 16:51:44 +0100

    TDD

--- spec/controller/movies_controller_spec.rb
+++ spec/controller/movies_controller_spec.rb
@@ -1,14 +1,23 @@
 require 'rspec'
 require_relative 'movies_controller_spec'
+# require_relative 'movies_controller'
+# require_relative 'app/controller/movies_controller'
 # require 'movies_controller'
 # require '../spec/movies_controller_rspec'
 # require '../app/controllers/movies_controller.rb'
 
-context 'index' do 
-  it 'Should display index of movies' do
-    expect(index(@movies)).to include(Movie.all)
+# context 'index' do 
+#   it 'Should display index of movies' do
+#     expect(index(@movies)).to include(Movie.all)
+#   end
+# end
+context 'MoviesController' do
+  context '#index' do 
+    it 'Should display index of movies' do
+      expect(index(@movies)).to include(Movie.all)
+    end
   end
-end
+end 
 
 # describe "#index" do
 #   it "Should display index of movies" do
@@ -21,5 +30,14 @@ end
 # describe 'index' do 
 #   it 'Should display index of movies' do
 #     expect(index(movies)).to include(Movie.all)
+#   end
+# end
+
+# describe MoviesController do 
+#   describe '#index' do
+#     it 'returns the index of Movies"' do
+#       movie = Movie.new
+#       expect(movie.index).to eql(Movie.all)
+#     end
 #   end
 # end
