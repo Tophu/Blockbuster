66eab6ad034f69f27cb76d4d29576e1c92df7425
Luis Ramirez <sdaewa22@gmail.com>
2019-07-09 15:04:43 +0100

    testing TDD

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,17 @@
 class OrdersController < ApplicationController
+  def initialize
+  end
+
   def index
+    @orders = Order.all
   end
 
   def show
+    @order = current_order.find(params[:id])
   end
 
   def new
+    @orders = Order.new
   end
 
   def edit
