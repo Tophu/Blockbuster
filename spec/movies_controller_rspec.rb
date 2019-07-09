6724c7b10b0bfad20361db0f60d447e7e245f9c8
Phuong To <tophu95@gmail.com>
2019-07-09 15:35:08 +0100

    TDD failed test is working

--- spec/movies_controller_rspec.rb
+++ spec/movies_controller_rspec.rb
@@ -1,4 +1,5 @@
-require 'movies_controller'
+# require '../spec/movies_controller_rspec'
+require_relative 'movies_controller_rspec'
 # require '../app/controllers/movies_controller.rb'
 
 context 'index' do 
@@ -12,5 +13,11 @@ end
 #     actual = "Index"
 #     expected "Movies"
 #     expect(actual).to eq(expected)
+#   end
+# end
+
+# context 'index' do 
+#   it 'Should display index of movies' do
+#     expect(index(movies)).to include(Movies.all)
 #   end
 # end
