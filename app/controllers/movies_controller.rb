a716393c4e2f70dbd7b1e16982e555df933f3ef1
Phuong To <tophu95@gmail.com>
2019-07-09 14:11:58 +0100

    writing methods and tests for movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,9 @@
 class MoviesController < ApplicationController
   def index
+    @movies = Movie.all
   end
 
   def show
+    @movie = current_movie.find(params[:id])
   end
 end
---
+++ spec/movies_controller_rspec.rb
@@ -0,0 +1,6 @@
+require 'movies_controller'
+
+context 'index' do 
+  it 'Should display index of movies' do
+    expect(index )
+end
