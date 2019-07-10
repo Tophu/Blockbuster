5000abff73f0a25d048fb037e1725169abcbbf21
Luis Ramirez <sdaewa22@gmail.com>
2019-07-10 13:08:50 +0100

    roots issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,11 @@
-
+Rails.application.routes.draw do
   get "orders/index"
   get "orders/show"
   get "orders/new"
   get "orders/edit"
   get "orders/destroy"
-  get "order/index"
-  get "order/show"
-  get "order/new"
-  get "order/edit"
-  get "order/destroy"
-  get "order/index"
-  get "order/show"
-  get "order/new"
-  get "order/edit"
   get "orders/reviews"
   get "orders/movies"
-
   devise_for :users
   root to: "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
