60020a78a90e8ea492958befb032512610e91559
Phuong To <tophu95@gmail.com>
2019-07-10 15:51:59 +0100

    resolving error

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_07_10_134037) do
-=======
-ActiveRecord::Schema.define(version: 2019_07_09_141411) do
->>>>>>> 4e9defea52f9c7da9b83cfe3c0660b2ff0a14042
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
