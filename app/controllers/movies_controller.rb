ed0061202f53081af5751029afaecd6c446e19fc
Phuong To <tophu95@gmail.com>
2019-07-10 17:08:15 +0100

    working on movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,14 +1,20 @@
 class MoviesController < ApplicationController
 
-  # def initialize
-  #   @movies = movies
-  # end
-
   def index
     @movies = Movie.all
   end
 
   def show
     @movie = current_movie.find(params[:id])
+  end
+
+  private
+
+  def movies_params
+    params.require(:movie).permit(:photo, :title, :description, :year, :genre)
+  end
+
+  def find_movie
+    @movie = Movie.find(params[:id])
   end
 end
