6de1af5c8d063b466be65f25268054dc62101a59
Luis Ramirez <sdaewa22@gmail.com>
2019-07-11 18:04:24 +0100

    added column payment to orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_11_164918) do
+ActiveRecord::Schema.define(version: 2019_07_11_170308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2019_07_11_164918) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "payment"
   end
 
   create_table "users", force: :cascade do |t|
