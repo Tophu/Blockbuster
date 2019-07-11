2e5ce9f8b4860f5e1255acf004e2afab8c64b92e
Chai Chai <c.chai@hotmail.co.uk>
2019-07-11 22:19:53 +0100

    migration, schema, seed, routes, gem.lock

--- db/migrate/20190711164736_create_baskets.rb
+++
@@ -1,8 +0,0 @@
-class CreateBaskets < ActiveRecord::Migration[6.0]
-  def change
-    create_table :baskets do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20190711170308_add_column_payment_to_order.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnPaymentToOrder < ActiveRecord::Migration[6.0]
-  def change
-    add_column :orders, :payment, :string
-  end
-end
