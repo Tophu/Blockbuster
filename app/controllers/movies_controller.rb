85bb30eb3949cb7b4cc410b75796504a1298974e
Luis Ramirez <sdaewa22@gmail.com>
2019-07-11 16:46:33 +0100

    customized routes for movies and index view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,20 @@
 class MoviesController < ApplicationController
   def index
+    @movies = Movie.all
   end
 
   def show
+    @movie = Movie.find(params[:id])
   end
 
   def new
+    @movie = Movie.new
   end
 
   def create
+    @movie = Movie.new(movies_params)
+    if @movie.save!
+      redirect_to index_path(@Movie.all)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'movies/index'
-  get 'movies/show'
-  get 'movies/new'
-  get 'movies/create'
+  get "movies", to: "movies#index", as: "movies"
+  get "movies/:id", to: "movies#show", as: "movie"
+  get "movies/new", to: "movies#new", as: "new"
+  get "movies/create"
   devise_for :users
-  root to: "pages#home"
+  root to: "movies#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
