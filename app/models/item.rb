c3f576d274b097ccdeab18007cfe50bb13957a01
Luis Ramirez <sdaewa22@gmail.com>
2019-07-11 17:39:05 +0100

    added validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  validates :quantity, presence: true, numericality: true
+  validates :movie_id, :basket_id, :order_id, presence: true, numericality: true, uniqueness: true
 end
