537187d8ec9fd4ee9edee557ae9dbfaa07e9bad0
Luis Ramirez <sdaewa22@gmail.com>
2019-07-11 18:49:55 +0100

    customized routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,23 @@
 Rails.application.routes.draw do
-  get 'orders/index'
-  get 'orders/show'
-  get 'orders/new'
-  get 'basket/show'
-  get "movies", to: "movies#index", as: "movies"
-  get "movies/:id", to: "movies#show", as: "movie", id: /\d+/ #only numeric digits
-  get "movies/new", to: "movies#new", as: "new_movie"
-  get "movies/create"
+
+  # get "movies", to: "movies#index", as: "movies"
+  # get "movies/:id", to: "movies#show", as: "movie", id: /\d+/ #only numeric digits
+  # get "movies/new", to: "movies#new", as: "new_movie"
+  # get "movies/create"
+  resources :movies
+  resources :orders
+
+  get "carts/:id" => "carts#show", as: "cart"
+  delete "carts/:id" => "carts#destroy"
+
+  post "items/:id/add" => "items#add_quantity", as: "item_add"
+  post "items/:id/reduce" => "items#reduce_quantity", as: "item_reduce"
+  post "items" => "items#create"
+  get "items/:id" => "items#show", as: "item"
+  delete "items/:id" => "items#destroy"
+
   devise_for :users
+
   root to: "movies#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
