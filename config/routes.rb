f1f802507db90e677061406b1c6c5c4ec4bbbef5
Luis Ramirez <sdaewa22@gmail.com>
2019-07-11 17:00:18 +0100

    constraining route and displaying by id

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "movies", to: "movies#index", as: "movies"
-  get "movies/:id", to: "movies#show", as: "movie"
+  get "movies/:id", to: "movies#show", as: "movie", id: /\d+/ #only numeric digits
   get "movies/new", to: "movies#new", as: "new"
   get "movies/create"
   devise_for :users
