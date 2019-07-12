2f8860cd7f76b3b8b5d8c3f902df4046e87f2f64
Phuong To <tophu95@gmail.com>
2019-07-12 16:32:50 +0100

    basket redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def current_basket
     if session[:basket_id]
-      cart = Basket.find_by(:id => session[:basket_id])
+      basket = Basket.find_by(:id => session[:basket_id])
       if basket.present?
         @current_basket = basket
       else
